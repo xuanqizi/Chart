4d88bb565d6b9e2ea766feddc8d4fe0c399ff69f
xuanqizi <1318135982@qq.com>
2021-05-09 11:26:19 +0800

    将vue 3 项目转为vue 2

---
+++ chart/src/components/Chart.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        HelloWorld
+        <LineChart :chartData="chartData" />
+    </div>
+</template>
+
+<script>
+import LineChart from './LineChart.vue'
+
+export default {
+    name: 'Chart',
+    components: {
+        LineChart
+    },
+    data() {
+        return {
+            chartData: null,
+        }
+    },
+    mounted() {
+        this.fillData();
+    },
+    methods: {
+        fillData() {
+            this.chartData = {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'My First Dataset',
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    fill: false,
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: 0.1
+                }]
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ chart/src/components/LineChart.vue
@@ -0,0 +1,30 @@
+<script>
+import { Line, mixins } from 'vue-chartjs';
+
+const { reactiveProp } = mixins;
+
+const options = {
+    plugins: {
+        zoom: {
+            enabled: true,      // 开启缩放功能
+            mode: 'xy',         // xy轴均可缩放
+        },
+        pan: {
+            enabled: true,      // 开启平移功能
+            mode: 'xy',         // xy轴均可平移
+        }
+    }
+};
+
+export default {
+    extends: Line,
+    mixins: [reactiveProp],
+    props: ['options'],
+    mounted() {
+        // this.chartData 在 mixin 创建
+        this.renderData(this.chartData, options);
+    }
+}
+</script>
+
+<style></style>
