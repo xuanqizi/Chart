eb6fe0c2bb6d3b45f77ea8f26c3be3d46c62b1ef
xuanqizi <1318135982@qq.com>
2021-05-18 11:44:05 +0800

    Revert "Merge branch 'ui'"
    This reverts commit e29167d5b19e69e65e8045b137658ee8847f6168, reversing
    changes made to b0b2fdfa2f166cf14c6577bc68d8a4b50c6e67cf.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,36 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
-    private static final String ALLOW_HIDPI_KEY = "prism.allowhidpi";   // 配置是否允许高DPI缩放的系统配置key
+/**
+ * 主类，继承Application类，用于启动应用程序
+ * @author Zhenxi Chen
+ * @date 2021/04/17
+ */
+public class Main extends Application {
+
+    private static final String appTitle = "Chart";     // 应用程序的标题
+    private static final int appWidth = 1920;       // 应用程序宽度
+    private static final int appHeight = 1000;      // 应用程序高度
+    private static final String fxmlFileName = "main.fxml";     // 主程序的fxml文件名
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        setUserAgentStylesheet(STYLESHEET_CASPIAN);
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(fxmlFileName));
+            Scene scene = new Scene(root, appWidth, appHeight);
+            primaryStage.setTitle(appTitle);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
 
     public static void main(String[] args) {
-        System.setProperty(ALLOW_HIDPI_KEY, "false");
-        Application.launch(MainApplication.class, args);
+        launch(args);
     }
 }
