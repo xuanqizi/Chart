84a177e7b2f6de038ed81708caaa74046e222735
xuanqizi <1318135982@qq.com>
2021-05-20 01:07:26 +0800

    添加了部分测试用例，并添加项目测试报告

--- src/test/java/utils/FileUtilTest.java
+++ src/test/java/utils/FileUtilTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
 public class FileUtilTest {
 
     @Test
-    public void readBufferAsShort() {
+    public void readBufferAsShortInBigEndian() {
         short[] expected = new short[]{1, 30, 570, 2333, 16250, -16110};
         byte[] input = new byte[] {
                 0x00, 0x01,
@@ -20,6 +20,19 @@ public class FileUtilTest {
                 -0x3f, 0x12
         };
         short[] res = FileUtil.readBufferAsShort(input, 12, ByteOrder.BIG_ENDIAN);
+        assertArrayEquals(expected, res);
+    }
+
+    @Test
+    public void readBufferAsShortInLittleEndian() {
+        byte[] input = new byte[] {
+                0x00, 0x01,
+                0x00, 0x1e,
+                0x02, 0x3a,
+                0x09, 0x1d,
+        };
+        short[] expected = new short[] {256, 7680, 14850, 7433};
+        short[] res = FileUtil.readBufferAsShort(input, 8, ByteOrder.LITTLE_ENDIAN);
         assertArrayEquals(expected, res);
     }
 }
